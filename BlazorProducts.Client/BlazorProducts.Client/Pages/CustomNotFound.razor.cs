508857a8f3db442aeb5fb3f33daf3e0260417404
Marinko <marinkospasojevic@gmail.com>
2020-10-19 08:36:52 +0200

    Initial Commit

---
+++ BlazorProducts.Client/BlazorProducts.Client/Pages/CustomNotFound.razor.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorProducts.Client.Pages
+{
+	public partial class CustomNotFound
+	{
+		[Inject]
+		public NavigationManager NavigationManager { get; set; }
+
+		public void NavigateToHome()
+		{
+			NavigationManager.NavigateTo("/");
+		}
+	}
+}
