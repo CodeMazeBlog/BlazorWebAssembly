303bb30d9c09e03455f06c5c83f64da3fb3be894
Marinko <marinkospasojevic@gmail.com>
2021-02-15 12:06:24 +0100

    Initial Commit

--- BlazorProducts.Server/BlazorProducts.Server/Startup.cs
+++ BlazorProducts.Server/BlazorProducts.Server/Startup.cs
@@ -13,59 +13,66 @@ using System.IO;
 namespace BlazorProducts.Server
 {
 	public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
+	{
+		public Startup(IConfiguration configuration)
+		{
+			Configuration = configuration;
+		}
 
-        public IConfiguration Configuration { get; }
+		public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddCors(policy =>
-            {
-                policy.AddPolicy("CorsPolicy", opt => opt
-                .AllowAnyOrigin()
-                .AllowAnyHeader()
-                .AllowAnyMethod()
-                .WithExposedHeaders("X-Pagination"));
-            });
+		// This method gets called by the runtime. Use this method to add services to the container.
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.AddCors(policy =>
+			{
+				policy.AddPolicy("CorsPolicy", opt => opt
+				.AllowAnyOrigin()
+				.AllowAnyHeader()
+				.AllowAnyMethod()
+				.WithExposedHeaders("X-Pagination"));
+			});
 
-            services.AddDbContext<ProductContext>(opt => opt.UseSqlServer(Configuration.GetConnectionString("sqlConnection")));
+			services.AddDbContext<ProductContext>(opt => opt.UseSqlServer(Configuration.GetConnectionString("sqlConnection")));
 
-            services.AddScoped<IProductRepository, ProductRepository>();
+			services.AddScoped<IProductRepository, ProductRepository>();
 
-            services.AddControllers();
-        }
+			services.AddControllers();
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+			services.AddRazorPages();
+		}
 
-            app.UseHttpsRedirection();
-            app.UseCors("CorsPolicy");
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+		{
+			if (env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+				app.UseWebAssemblyDebugging();
+			}
 
-            app.UseStaticFiles(new StaticFileOptions
-            {
-                FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(),
-                @"StaticFiles")),
-                RequestPath = new PathString("/StaticFiles")
-            });
+			app.UseHttpsRedirection();
+			app.UseCors("CorsPolicy");
 
-            app.UseRouting();
+			app.UseBlazorFrameworkFiles();
+			app.UseStaticFiles();
+			app.UseStaticFiles(new StaticFileOptions
+			{
+				FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(),
+				@"StaticFiles")),
+				RequestPath = new PathString("/StaticFiles")
+			});
 
-            app.UseAuthorization();
+			app.UseRouting();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
-        }
-    }
+			app.UseAuthorization();
+
+			app.UseEndpoints(endpoints =>
+			{
+				endpoints.MapControllers();
+				endpoints.MapRazorPages();
+				endpoints.MapFallbackToFile("index.html");
+			});
+		}
+	}
 }
