c84f2215d9fc3ff2bfd08c01cbc8d9cf0b7dcbdb
MarinkoSpasojevic <marinkospasojevic@gmail.com>
2022-04-15 11:44:31 +0200

    Source Code for .NET 6

---
+++ .NET 6/BlazorProducts.Client/BlazorProducts.Client/Pages/CreateProduct.razor.cs
@@ -0,0 +1,65 @@
+﻿using Blazored.Toast.Services;
+using BlazorProducts.Client.HttpInterceptor;
+using BlazorProducts.Client.HttpRepository;
+using Entities.Models;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+
+namespace BlazorProducts.Client.Pages
+{
+    public partial class CreateProduct : IDisposable
+	{
+		private Product _product = new Product();
+		private EditContext? _editContext;
+		private bool formInvalid = true;
+
+		[Inject]
+		public IProductHttpRepository? ProductRepo { get; set; }
+
+		[Inject]
+		public HttpInterceptorService? Interceptor { get; set; }
+
+		[Inject]
+		public IToastService? ToastService { get; set; }
+
+		protected override void OnInitialized()
+		{
+			_editContext = new EditContext(_product);
+			_editContext.OnFieldChanged += HandleFieldChanged;
+			Interceptor.RegisterEvent();
+		}
+
+		private void HandleFieldChanged(object? sender, FieldChangedEventArgs e)
+		{
+			formInvalid = !_editContext.Validate();
+			StateHasChanged();
+		}
+
+		private async Task Create()
+		{
+			await ProductRepo.CreateProduct(_product);
+
+			ToastService.ShowSuccess($"Action successful. " +
+				$"Product \"{_product.Name}\" successfully added.");
+			_product = new Product();
+			_editContext.OnValidationStateChanged += ValidationChanged;
+			_editContext.NotifyValidationStateChanged();
+		}
+
+		private void ValidationChanged(object? sender, ValidationStateChangedEventArgs e)
+		{
+			formInvalid = true;
+			_editContext.OnFieldChanged -= HandleFieldChanged;
+			_editContext = new EditContext(_product);
+			_editContext.OnFieldChanged += HandleFieldChanged;
+			_editContext.OnValidationStateChanged -= ValidationChanged;
+		}
+
+		public void Dispose()
+		{
+			Interceptor.DisposeEvent();
+			_editContext.OnFieldChanged -= HandleFieldChanged;
+			_editContext.OnValidationStateChanged -= ValidationChanged;
+		}
+	}
+}
---
+++ .NET 6/BlazorProducts.Client/BlazorProducts.Client/Pages/Products.razor.cs
@@ -0,0 +1,67 @@
+﻿using BlazorProducts.Client.HttpInterceptor;
+using BlazorProducts.Client.HttpRepository;
+using Entities.Models;
+using Entities.RequestFeatures;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorProducts.Client.Pages
+{
+    public partial class Products : IDisposable
+	{
+		public List<Product> ProductList { get; set; } = new List<Product>();
+		public MetaData MetaData { get; set; } = new MetaData();
+
+		private ProductParameters _productParameters = new ProductParameters();
+
+		[Inject]
+		public IProductHttpRepository ProductRepo { get; set; }
+
+		[Inject]
+		public HttpInterceptorService Interceptor { get; set; }
+
+		protected async override Task OnInitializedAsync()
+		{
+			Interceptor.RegisterEvent();
+			await GetProducts();
+		}
+
+		private async Task SelectedPage(int page)
+		{
+			_productParameters.PageNumber = page;
+			await GetProducts();
+		}
+
+		private async Task GetProducts()
+		{
+			var pagingResponse = await ProductRepo.GetProducts(_productParameters);
+
+			ProductList = pagingResponse.Items;
+			MetaData = pagingResponse.MetaData;
+		}
+
+		private async Task SetPageSize(int pageSize)
+		{
+			_productParameters.PageSize = pageSize;
+			_productParameters.PageNumber = 1;
+
+			await GetProducts();
+		}
+
+		private async Task SearchChanged(string searchTerm)
+		{
+			_productParameters.PageNumber = 1;
+			_productParameters.SearchTerm = searchTerm;
+
+			await GetProducts();
+		}
+
+		private async Task SortChanged(string orderBy)
+		{
+			_productParameters.OrderBy = orderBy;
+
+			await GetProducts();
+		}
+
+		public void Dispose() => Interceptor.DisposeEvent();
+	}
+}
