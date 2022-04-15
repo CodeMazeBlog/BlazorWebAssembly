6648db4c234dc91eb72d04368a94684030917720
MarinkoSpasojevic <marinkospasojevic@gmail.com>
2022-04-15 11:49:10 +0200

    Source Code for .NET 6

---
+++ .NET 6/BlazorProducts.Client/BlazorProducts.Client/Pages/ProductDetails.razor.cs
@@ -0,0 +1,22 @@
+﻿using BlazorProducts.Client.HttpRepository;
+using Entities.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorProducts.Client.Pages
+{
+    public partial class ProductDetails
+	{
+		public Product Product { get; set; } = new Product();
+
+		[Inject]
+		public IProductHttpRepository? ProductRepo { get; set; }
+
+		[Parameter]
+		public Guid ProductId { get; set; }
+
+		protected async override Task OnInitializedAsync()
+		{
+			Product = await ProductRepo.GetProduct(ProductId);
+		}
+	}
+}
