42f532c26e4d8ba3a222d7a8d8cf09d8f727fc76
MarinkoSpasojevic <marinkospasojevic@gmail.com>
2022-04-15 11:46:05 +0200

    Source Code for .NET 6

---
+++ .NET 6/BlazorProducts.Server/BlazorProducts.Server/Controllers/ProductsController.cs
@@ -0,0 +1,50 @@
+﻿using System.Text.Json;
+using BlazorProducts.Server.Repository;
+using Entities.Models;
+using Entities.RequestFeatures;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorProducts.Server.Controllers
+{
+    [Route("api/products")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductRepository _repo;
+
+        public ProductsController(IProductRepository repo)
+        {
+            _repo = repo;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get([FromQuery] ProductParameters productParameters)
+        {
+            var products = await _repo.GetProducts(productParameters);
+
+            Response.Headers.Add("X-Pagination", JsonSerializer.Serialize(products.MetaData));
+
+            return Ok(products);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetProduct(Guid id)
+        {
+            var product = await _repo.GetProduct(id);
+            return Ok(product);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateProduct([FromBody] Product product)
+        {
+            if (product is null)
+                return BadRequest("Product has not been set");
+
+            //model validation
+
+            await _repo.CreateProduct(product);
+
+            return CreatedAtAction(nameof(GetProduct), new { id = product.Id }, product);
+        }
+    }
+}
