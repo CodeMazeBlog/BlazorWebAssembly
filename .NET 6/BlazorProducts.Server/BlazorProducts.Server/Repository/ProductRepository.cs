0aaabf0ae67a3d08585ca9f7e4174e757000501f
MarinkoSpasojevic <marinkospasojevic@gmail.com>
2022-04-15 11:28:55 +0200

    Source Code for .NET 6

---
+++ .NET 6/BlazorProducts.Server/BlazorProducts.Server/Repository/ProductRepository.cs
@@ -0,0 +1,21 @@
+﻿using BlazorProducts.Server.Context;
+using Entities.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorProducts.Server.Repository
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly ProductContext _context;
+
+        public ProductRepository(ProductContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Product>> GetProducts() => await _context.Products.ToListAsync();
+
+        public async Task<Product> GetProduct(Guid id) =>
+            await _context.Products.FirstOrDefaultAsync(p => p.Id.Equals(id));
+    }
+}
