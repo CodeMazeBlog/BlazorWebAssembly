196120a52e95a4ef646e6a07947c14a4bfc5a05c
MarinkoSpasojevic <marinkospasojevic@gmail.com>
2022-04-15 11:26:55 +0200

    Source Code for .NET 6

---
+++ .NET 6/BlazorProducts.Client/BlazorProducts.Client/Program.cs
@@ -0,0 +1,13 @@
+using BlazorProducts.Client;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Logging.AddConfiguration(builder.Configuration.GetSection("Logging"));
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+await builder.Build().RunAsync();
