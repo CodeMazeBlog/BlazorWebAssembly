da1a600c224cc9452c6cc26880ddaf757d576880
MarinkoSpasojevic <marinkospasojevic@gmail.com>
2022-04-15 11:30:14 +0200

    Source Code for .NET 6

---
+++ .NET 6/BlazorProducts.Client/BlazorProducts.Client/Components/ProductTable.razor.cs
@@ -0,0 +1,11 @@
+﻿using Entities.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorProducts.Client.Components
+{
+    public partial class ProductTable
+	{
+		[Parameter]
+		public List<Product> Products { get; set; }
+	}
+}
