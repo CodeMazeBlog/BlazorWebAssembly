b18af2208ecf602e7f6297780f36f668f544727c
MarinkoSpasojevic <marinkospasojevic@gmail.com>
2022-04-15 11:36:40 +0200

    Source Code for .NET 6

---
+++ .NET 6/BlazorProducts.Client/BlazorProducts.Client/Components/Home.razor.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorProducts.Client.Components
+{
+    public partial class Home
+    {
+		[Parameter]
+		public string? Title { get; set; }
+
+		[Parameter(CaptureUnmatchedValues = true)]
+		public Dictionary<string, object>? AdditionalAttributes { get; set; }
+
+		[CascadingParameter(Name = "HeadingColor")]
+		public string? Color { get; set; }
+
+		[Parameter]
+		public RenderFragment? VisitShopContent { get; set; }
+	}
+}
