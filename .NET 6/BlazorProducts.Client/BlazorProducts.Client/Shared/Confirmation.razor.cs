02e17154a62b5fd1f055889ae0b7cb47a2e746ae
MarinkoSpasojevic <marinkospasojevic@gmail.com>
2022-04-15 11:50:20 +0200

    Source Code for .NET 6

---
+++ .NET 6/BlazorProducts.Client/BlazorProducts.Client/Shared/Confirmation.razor.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorProducts.Client.Shared
+{
+    public partial class Confirmation
+	{
+		private string? _modalDisplay;
+		private bool _showBackdrop;
+
+		[Parameter]
+		public string? BodyMessage { get; set; }
+
+		[Parameter]
+		public EventCallback OnOKClicked { get; set; }
+
+		public void Show()
+		{
+			_modalDisplay = "block;";
+			_showBackdrop = true;
+			StateHasChanged();
+		}
+
+		public void Hide()
+		{
+			_modalDisplay = "none;";
+			_showBackdrop = false;
+			StateHasChanged();
+		}
+	}
+}
