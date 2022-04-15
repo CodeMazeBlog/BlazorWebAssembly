13c78e09ea76fb7eec8b2295a54d8fdafec2a9d0
MarinkoSpasojevic <marinkospasojevic@gmail.com>
2022-04-15 11:51:25 +0200

    Source Code for .NET 6

---
+++ .NET 6/BlazorProducts.Client/BlazorProducts.Client/Pages/ReportError.razor.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorProducts.Client.Pages
+{
+    public partial class ReportError
+	{
+		[Parameter]
+		public int ErrorCode { get; set; }
+
+		[Parameter]
+		public string? ErrorDescription { get; set; }
+	}
+}
