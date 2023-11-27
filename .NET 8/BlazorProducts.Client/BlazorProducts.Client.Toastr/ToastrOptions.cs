ed463a244d780b259b3360eb2cfc6c8b97b55ff1
Code Maze <contact@code-maze.com>
2023-11-27 13:42:23 +0100

    .NET 8 Source code update

---
+++ .NET 8/BlazorProducts.Client/BlazorProducts.Client.Toastr/ToastrOptions.cs
@@ -0,0 +1,27 @@
+﻿using BlazorProducts.Client.Toastr.CustomConverters;
+using BlazorProducts.Client.Toastr.Enumerations;
+using System.Text.Json.Serialization;
+
+namespace BlazorProducts.Client.Toastr
+{
+    public class ToastrOptions
+	{
+		[JsonConverter(typeof(CustomEnumDescriptionConverter<ToastrPosition>))]
+		[JsonPropertyName("positionClass")]
+		public ToastrPosition Position { get; set; }
+
+		[JsonConverter(typeof(CustomEnumDescriptionConverter<ToastrHideMethod>))]
+		[JsonPropertyName("hideMethod")]
+		public ToastrHideMethod HideMethod { get; set; }
+
+		[JsonConverter(typeof(CustomEnumDescriptionConverter<ToastrShowMethod>))]
+		[JsonPropertyName("showMethod")]
+		public ToastrShowMethod ShowMethod { get; set; }
+
+		[JsonPropertyName("closeButton")]
+		public bool CloseButton { get; set; }
+
+		[JsonPropertyName("hideDuration")]
+		public int HideDuration { get; set; }
+	}
+}
---
+++ .NET 8/BlazorProducts.Client/BlazorProducts.Client/Shared/ImageUpload.razor.cs
@@ -0,0 +1,47 @@
+﻿using BlazorProducts.Client.HttpRepository;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using System.Net.Http.Headers;
+
+namespace BlazorProducts.Client.Shared
+{
+    public partial class ImageUpload
+	{
+		private string _fileUploadMessage = "No file chosen";
+
+		[Parameter]
+		public string? ImgUrl { get; set; }
+
+		[Parameter]
+		public EventCallback<string> OnChange { get; set; }
+
+		[Inject]
+		public IProductHttpRepository? Repository { get; set; }
+
+		private async Task HandleSelected(InputFileChangeEventArgs e)
+		{
+			var imageFile = e.File;
+			_fileUploadMessage = string.Empty;
+
+			if (imageFile == null)
+				return;
+
+			_fileUploadMessage += $"{imageFile.Name}";
+
+			var resizedFile = await imageFile.RequestImageFileAsync("image/png", 300, 500);
+
+			using (var ms = resizedFile.OpenReadStream(resizedFile.Size))
+			{
+				var content = new MultipartFormDataContent();
+				content.Headers.ContentDisposition = 
+					new ContentDispositionHeaderValue("form-data");
+				content.Add(new StreamContent(ms, Convert.ToInt32(resizedFile.Size)),
+					"image", imageFile.Name);
+
+				ImgUrl = await Repository.UploadProductImage(content);
+
+				await OnChange.InvokeAsync(ImgUrl);
+			}
+		}
+	}
+}
