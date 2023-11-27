fdb7a119b80f2daf53b6757c969442b2fd7ccf3a
Code Maze <contact@code-maze.com>
2023-11-27 13:38:06 +0100

    .NET 8 Source code update

---
+++ .NET 8/BlazorProducts.Client/BlazorProducts.Client/HttpRepository/IProductHttpRepository.cs
@@ -0,0 +1,14 @@
+﻿using BlazorProducts.Client.Features;
+using Entities.Models;
+using Entities.RequestFeatures;
+
+namespace BlazorProducts.Client.HttpRepository
+{
+    public interface IProductHttpRepository
+	{
+		Task<PagingResponse<Product>> GetProducts(ProductParameters productParameters);
+		Task<Product> GetProduct(Guid id);
+		Task CreateProduct(Product product);
+		Task<string> UploadProductImage(MultipartFormDataContent content);
+	}
+}
---
+++ .NET 8/BlazorProducts.Server/BlazorProducts.Server/Repository/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using BlazorProducts.Server.Paging;
+using Entities.Models;
+using Entities.RequestFeatures;
+
+namespace BlazorProducts.Server.Repository
+{
+    public interface IProductRepository
+    {
+        Task<PagedList<Product>> GetProducts(ProductParameters productParameters);
+        Task<Product> GetProduct(Guid id);
+        Task CreateProduct(Product product);
+    }
+}
