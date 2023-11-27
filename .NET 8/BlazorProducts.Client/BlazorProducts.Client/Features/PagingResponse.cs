3482a85c2c3deeec23d9e3e55639c0ef2db5112b
Code Maze <contact@code-maze.com>
2023-11-27 13:37:02 +0100

    .NET 8 Source code update

---
+++ .NET 8/BlazorProducts.Client/BlazorProducts.Client/Features/PagingResponse.cs
@@ -0,0 +1,10 @@
+﻿using Entities.RequestFeatures;
+
+namespace BlazorProducts.Client.Features
+{
+    public class PagingResponse<T> where T : class
+	{
+		public List<T>? Items { get; set; }
+		public MetaData? MetaData { get; set; }
+	}
+}
