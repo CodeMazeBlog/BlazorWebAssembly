35d9737adc9bf093a893964c3df7e75d9586d97c
Code Maze <contact@code-maze.com>
2023-11-27 13:35:52 +0100

    .NET 8 Source code update

---
+++ .NET 8/BlazorProducts.Server/BlazorProducts.Server/Repository/ProductRepository.cs
@@ -0,0 +1,33 @@
+﻿using BlazorProducts.Server.Context;
+using BlazorProducts.Server.Paging;
+using BlazorProducts.Server.Repository.RepositoryExtensions;
+using Entities.Models;
+using Entities.RequestFeatures;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorProducts.Server.Repository
+{
+	public class ProductRepository : IProductRepository
+	{
+		private readonly ProductContext _context;
+
+		public ProductRepository(ProductContext context)
+		{
+			_context = context;
+		}
+
+		public async Task<PagedList<Product>> GetProducts(ProductParameters productParameters)
+		{
+			var products = await _context.Products
+				.Search(productParameters.SearchTerm)
+				.Sort(productParameters.OrderBy)
+				.ToListAsync();
+			
+			return PagedList<Product>
+				.ToPagedList(products, productParameters.PageNumber, productParameters.PageSize);
+		}
+
+		public async Task<Product> GetProduct(Guid id) =>
+			await _context.Products.FirstOrDefaultAsync(p => p.Id.Equals(id));
+	}
+}
