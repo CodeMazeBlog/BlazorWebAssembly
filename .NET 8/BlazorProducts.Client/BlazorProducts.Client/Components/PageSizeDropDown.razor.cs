db44198175aa6a27bd9dc076a547191a2e85c248
Code Maze <contact@code-maze.com>
2023-11-27 13:37:31 +0100

    .NET 8 Source code update

---
+++ .NET 8/BlazorProducts.Client/BlazorProducts.Client/Components/PageSizeDropDown.razor.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorProducts.Client.Components
+{
+    public partial class PageSizeDropDown
+	{
+		[Parameter]
+		public EventCallback<int> SelectedPageSize { get; set; }
+
+		private async Task OnPageSizeChange(ChangeEventArgs eventArgs)
+		{
+			await SelectedPageSize.InvokeAsync(Int32.Parse(eventArgs.Value.ToString()));
+		}
+	}
+}
