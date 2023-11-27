ec83337d51dae2603c5a6cc0777ac5573740a40a
Code Maze <contact@code-maze.com>
2023-11-27 13:39:05 +0100

    .NET 8 Source code update

---
+++ .NET 8/BlazorProducts.Client/BlazorProducts.Client/Program.cs
@@ -0,0 +1,32 @@
+using Blazored.Toast;
+using BlazorProducts.Client;
+using BlazorProducts.Client.HttpInterceptor;
+using BlazorProducts.Client.HttpRepository;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Toolbelt.Blazor.Extensions.DependencyInjection;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+builder.Logging.AddConfiguration(builder.Configuration.GetSection("Logging"));
+
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddHttpClient("ProductsAPI", (sp, cl) =>
+{
+	cl.BaseAddress = new Uri("https://localhost:5011/api/");
+	cl.EnableIntercept(sp);
+});
+
+builder.Services.AddBlazoredToast();
+
+builder.Services.AddScoped(
+	sp => sp.GetService<IHttpClientFactory>().CreateClient("ProductsAPI"));
+
+builder.Services.AddHttpClientInterceptor();
+builder.Services.AddScoped<HttpInterceptorService>();
+
+builder.Services.AddScoped<IProductHttpRepository, ProductHttpRepository>();
+
+await builder.Build().RunAsync();
