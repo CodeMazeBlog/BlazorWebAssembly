21792fca89fbcfbc2d33cc37bd980bee3c224e1c
Code Maze <contact@code-maze.com>
2023-11-27 13:34:03 +0100

    .NET 8 Source code update

---
+++ .NET 8/BlazorProducts.Client/BlazorProducts.Client/HttpRepository/ProductHttpRepository.cs
@@ -0,0 +1,57 @@
+﻿using BlazorProducts.Client.Features;
+using Entities.Models;
+using Entities.RequestFeatures;
+using Microsoft.AspNetCore.WebUtilities;
+using System.Net.Http.Json;
+using System.Text.Json;
+
+namespace BlazorProducts.Client.HttpRepository
+{
+    public class ProductHttpRepository : IProductHttpRepository
+	{
+		private readonly HttpClient _client;
+		private readonly JsonSerializerOptions _options =
+			new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+
+		public ProductHttpRepository(HttpClient client)
+		{
+			_client = client;
+		}
+
+		public async Task<Product> GetProduct(Guid id)
+		{
+			var product = await _client.GetFromJsonAsync<Product>($"products/{id}");
+
+			return product;
+		}
+
+		public async Task<PagingResponse<Product>> GetProducts(ProductParameters productParameters)
+		{
+			var queryStringParam = new Dictionary<string, string>
+			{
+				["pageNumber"] = productParameters.PageNumber.ToString(),
+				["pageSize"] = productParameters.PageSize.ToString(),
+				["searchTerm"] = productParameters.SearchTerm == null ? string.Empty : productParameters.SearchTerm,
+				["orderBy"] = productParameters.OrderBy == null ? "" : productParameters.OrderBy
+			};
+
+			var response =
+				await _client.GetAsync(QueryHelpers.AddQueryString("products", queryStringParam));
+
+			var content = await response.Content.ReadAsStringAsync();
+			if (!response.IsSuccessStatusCode)
+			{
+				throw new ApplicationException(content);
+			}
+
+			var pagingResponse = new PagingResponse<Product>
+			{
+				Items = JsonSerializer.Deserialize<List<Product>>(content, _options),
+				MetaData = JsonSerializer.Deserialize<MetaData>(
+					response.Headers.GetValues("X-Pagination").First(), _options)
+			};
+
+			return pagingResponse;
+		}
+	}
+}
---
+++ .NET 8/BlazorProducts.Client/BlazorProducts.Client/Program.cs
@@ -0,0 +1,23 @@
+using BlazorProducts.Client;
+using BlazorProducts.Client.HttpRepository;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+builder.Logging.AddConfiguration(builder.Configuration.GetSection("Logging"));
+
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddHttpClient("ProductsAPI", cl =>
+{
+	cl.BaseAddress = new Uri("https://localhost:5011/api/");
+});
+
+builder.Services.AddScoped(
+	sp => sp.GetService<IHttpClientFactory>().CreateClient("ProductsAPI"));
+
+builder.Services.AddScoped<IProductHttpRepository, ProductHttpRepository>();
+
+await builder.Build().RunAsync();
